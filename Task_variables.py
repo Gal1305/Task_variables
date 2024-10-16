487487b4b0ead13a37dbf1d98c618665fc6d5995
Gal1305 <lollolloll12313@gmail.com>
2024-10-16 16:29:49 +0500

    Add files via upload

---
+++ Task_variables.py
@@ -0,0 +1,5 @@
+completed_tasks = 12
+lead_time_all_tasks = 1.5
+name_course = 'Python'
+time_for_one_task = (lead_time_all_tasks / completed_tasks)
+print('Курс:',name_course, ', всего задач: ', completed_tasks, ', затрачено часов: ', lead_time_all_tasks, ', среднее время выполнения одного задания: ', time_for_one_task, 'часа.')
